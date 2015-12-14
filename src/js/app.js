7c44a59f7d1bade783b36f24b2a50d3494a4cd4b
banzayche <dharm.serg@gmail.com>
2015-12-14 21:00:29 +0200

    refactoring

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,9 @@
 define(['kitchen', 'choose_image', 'jquery', 'jquery-ui', 'underscore', 'backbonewreqr', 'backbonebabysitter', 'backbone', 'backbonemarionette'], function(kitchenMainView, chooseImageView) {
 	var deferred = Marionette.Deferred();
 	var mainApp;
-	
-	_.result(deferred, 'promise').then(function (target) {
-	    // console.log("Hello, " + target + "!");
-	    mainApp.start();
-	});
 
-	var getReady = function(){
-		var default_paths_images = {
+	return new Promise(function(resolve, reject) {
+			var default_paths_images = {
 			k1: './src/images/kitchen/k1-beige.png',
 			k2: './src/images/kitchen/k2-beige.png',
 			k3: './src/images/kitchen/k3-beige.png',
@@ -42,23 +37,6 @@ define(['kitchen', 'choose_image', 'jquery', 'jquery-ui', 'underscore', 'backbon
 			$(mainApp.imagesListView.el).parent().hide();			
 		});
 
-		// function hide_show(){
-		// 	$(mainApp.imagesListView.el).hide();
-		// 	setTimeout(function() {
-		// 		console.log(mainApp.mainView.el);
-		// 		$(mainApp.mainView.el).parent().hide();
-		// 		$(mainApp.imagesListView.el).parent().show();
-		// 		// mainApp.getRegion('aRegion').show(imagesView);
-		// 	}, 2000);
-		// 	setTimeout(function() {
-		// 		console.log(mainApp.mainView.el);
-		// 		$(mainApp.imagesListView.el).hide();
-		// 		$(mainApp.mainView.el).show();
-		// 		// mainApp.getRegion('aRegion').show(layoutView);
-		// 	}, 2000);
-		// }
-
-
 		mainApp.vent.on("change_image", function(someData){
 			var image = someData();
 			mainApp.mainView.model.set({
@@ -77,6 +55,6 @@ define(['kitchen', 'choose_image', 'jquery', 'jquery-ui', 'underscore', 'backbon
 			$(mainApp.mainView.el).parent().hide(200);
 		});
 
-		deferred.resolve('true'); // asynchronous "Hello, world!"
-	}();	
+		resolve(mainApp); // asynchronous "Hello, world!"
+	});	
 });
